63e60146a9734e78831b71711cde6ab95668c06a
Varatharuban B <developer-at-398378596368>
2020-12-26 16:26:01 +0800

    Hystrix fall back services implemented

---
+++ hystrix-dashboard-service/src/test/java/com/rubanelearning/hystrixdashboard/HystrixDashboardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubanelearning.hystrixdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixDashboardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
